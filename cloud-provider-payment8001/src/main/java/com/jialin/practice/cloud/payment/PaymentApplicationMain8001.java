8aea07c7f58687fd2de5e8fbcf9b94d47bd51476
HillLi <cugb_ljl@126.com>
2022-05-21 21:50:49 +0800

    添加EnableDiscoveryClient注解

--- cloud-provider-payment8001/src/main/java/com/jialin/practice/cloud/payment/PaymentApplicationMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/jialin/practice/cloud/payment/PaymentApplicationMain8001.java
@@ -2,8 +2,10 @@ package com.jialin.practice.cloud.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentApplicationMain8001 {
 
     public static void main(String[] args) {
--- cloud-provider-payment8002/src/main/java/com/jialin/practice/cloud/payment/PaymentApplicationMain8002.java
+++ cloud-provider-payment8002/src/main/java/com/jialin/practice/cloud/payment/PaymentApplicationMain8002.java
@@ -2,8 +2,10 @@ package com.jialin.practice.cloud.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentApplicationMain8002 {
 
     public static void main(String[] args) {
